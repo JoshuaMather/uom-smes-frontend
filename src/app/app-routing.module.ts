f4e2676ba96a242be676b812a39babe44734b549
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2022-10-16 17:50:04 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { StudentPageComponent } from './student/student-page/student-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent,
+    children: [
+      { path: '', redirectTo: 'student/:id', pathMatch: 'full' },
+      { path: 'student/:id', component: StudentPageComponent },
+     ]
+     }
+];
+
+// const routes: Routes = [
+//   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+//   { path: 'dashboard', component: DashboardComponent },
+//   { path: 'detail/:id', component: HeroDetailComponent },
+//   { path: 'heroes', component: HeroesComponent }
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { StudentPageComponent } from './student/student-page/student-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginPageComponent,
+    StudentPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
