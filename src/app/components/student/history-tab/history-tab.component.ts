3eaa1bc121e01867fa5febd45ef3c717135aa692
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2022-11-23 14:49:56 +0000

    Add bar graph to history tab
    Also fix minor error with attendance compare graph

--- src/app/components/student/history-tab/history-tab.component.ts
+++ src/app/components/student/history-tab/history-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-history-tab',
@@ -13,16 +14,20 @@ export class HistoryTabComponent implements OnInit {
   @ViewChild('spotPaginator') spotPaginator!: MatPaginator;
   @ViewChild('gitPaginator') gitPaginator!: MatPaginator;
 
+  @ViewChild('barGraph', {static: true}) barGraph!: any;
+
   dataSourceBlackboard!: MatTableDataSource<any>;
   dataSourceSpot!: MatTableDataSource<any>;
   dataSourceGit!: MatTableDataSource<any>;
 
   history: any;
+  chart: any;
 
   constructor() { }
 
   ngOnInit(): void {
     this.groupHistory();
+    this.createBar();
   }
 
   ngAfterViewInit() {
@@ -52,6 +57,66 @@ export class HistoryTabComponent implements OnInit {
     this.dataSourceSpot = new MatTableDataSource(this.history.spot);
     this.dataSourceGit = new MatTableDataSource(this.history.git);
     console.log(this.dataSourceBlackboard);
+  }
+
+  createBar() {
+    let blackboard = [0,0,0,0,0,0,0,0,0,0,0,0];
+    let spot = [0,0,0,0,0,0,0,0,0,0,0,0];
+    let git = [0,0,0,0,0,0,0,0,0,0,0,0];
+    this.studentInfo.student_last.forEach((last: { type: string; week: number; }) => {
+      if(last.type=='blackboard') {
+        blackboard[last.week-1] += 1;
+      } else if(last.type=='spot') {
+        spot[last.week-1] += 1;
+      } else if(last.type=='git') {
+        git[last.week-1] += 1;
+      }
+    });
+
+    let historyDatasets = [];
+
+    historyDatasets.push({
+      label: 'Blackboard',
+      data: blackboard,
+    });
+    historyDatasets.push({
+      label: 'Spot',
+      data: spot,
+    });
+    historyDatasets.push({
+      label: 'Git',
+      data: git,
+    });
+
+    this.chart = new Chart(this.barGraph.nativeElement, {
+      type: 'bar',
+
+      data: {
+        labels: ['1','2','3','4','5','6','7','8','9','10','11','12'], 
+	       datasets: historyDatasets,
+      },
+      options: {
+        scales: {
+          y: {
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1,
+              },
+              title: {
+                display: true,
+                text: 'Amount'
+              }
+          },
+          x: {
+            title: {
+              display: true,
+              text: 'Week'
+            }
+        }
+      }
+      }
+      
+    });
   }
 
   click() {
