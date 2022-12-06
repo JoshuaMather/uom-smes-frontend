4ddd00fe6d99432ab7a2f2b5792a39590c9fdde3
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2022-12-06 13:34:32 +0000

    Add grades graph for course
    Graph shows current and predicted grade per course

--- src/app/components/student/grades-tab/grades-tab.component.ts
+++ src/app/components/student/grades-tab/grades-tab.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data/data.service';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-grades-tab',
@@ -8,9 +9,12 @@ import { DataService } from 'src/app/services/data/data.service';
 })
 export class GradesTabComponent implements OnInit {
   @Input() studentInfo : any;
+  @ViewChild('barGraph', {static: true}) barGraph!: any;
 
   user: any;
   courseAssignmentInfo: any;
+
+  chart: any;
 
   constructor(
     private data: DataService,
@@ -19,11 +23,12 @@ export class GradesTabComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.data.getUser();
     this.getGradeData();
+    this.createBar();
   }
 
   getGradeData() {
     let courseAssignmentData: any = [];
-    this.studentInfo.student_course.forEach((course: { course: { id: any; course_name: any; }; grades: { current: any; predict: any; }; }) => {
+    this.studentInfo.student_course.forEach((course: { course: { id: any; course_code: string; course_name: string; }; grades: { current: any; predict: any; }; }) => {
       let courseAssignments = this.studentInfo.student_assignment.filter((assignment: { assignment: { course: { id: any; }; }; }) => assignment.assignment.course.id === course.course.id);
 
       let assignmentData: any = [];
@@ -55,7 +60,7 @@ export class GradesTabComponent implements OnInit {
       })
 
       courseAssignmentData.push({
-        course: course.course.course_name,
+        course: course.course.course_code + ' ' + course.course.course_name,
         currentGrade: course.grades.current,
         predictedGrade: course.grades.predict,
         assignments: assignmentData,
@@ -65,4 +70,57 @@ export class GradesTabComponent implements OnInit {
     this.courseAssignmentInfo = courseAssignmentData;
   }
 
+  createBar() {
+    let courseLabel: string[] = [];
+    let currentGrades: any[] = [];
+    let predictedGrades: any[] = [];
+    let gradeDatasets = [];
+
+    this.courseAssignmentInfo.forEach((courseInfo: { course: string; currentGrade: any; predictedGrade: any; }) => {
+      courseLabel.push(courseInfo.course);
+      currentGrades.push(courseInfo.currentGrade);
+      predictedGrades.push(courseInfo.predictedGrade);
+    });
+
+    gradeDatasets.push(
+      {
+        label: 'Current Grades',
+        data: currentGrades
+      }, 
+      {
+        label: 'Predicted Grades',
+        data: predictedGrades
+      });
+
+    this.chart = new Chart(this.barGraph.nativeElement, {
+      type: 'bar',
+
+      data: {
+        labels: courseLabel, // courses 
+	       datasets: gradeDatasets, // predicted and average data
+      },
+      options: {
+        scales: {
+          y: {
+              max: 1,
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1,
+              },
+              title: {
+                display: true,
+                text: 'Grade'
+              }
+          },
+          x: {
+            title: {
+              display: true,
+              text: 'Course'
+            }
+        }
+      }
+      }
+      
+    });
+  }
 }
