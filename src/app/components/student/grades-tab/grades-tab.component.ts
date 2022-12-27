8bd33656ecc78ee069975c250ad337eddc33ab4d
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2022-12-27 17:26:28 +0000

    Add averages to student grades tab graph

--- src/app/components/student/grades-tab/grades-tab.component.ts
+++ src/app/components/student/grades-tab/grades-tab.component.ts
@@ -28,7 +28,9 @@ export class GradesTabComponent implements OnInit {
 
   getGradeData() {
     let courseAssignmentData: any = [];
-    this.studentInfo.student_course.forEach((course: { course: { id: any; course_code: string; course_name: string; }; grades: { current: any; predict: any; }; }) => {
+    this.studentInfo.student_course.forEach((course: { course: {
+      average_grades: any; id: any; tutor: any; course_code: string; course_name: string; 
+}; grades: { current: any; predict: any; }; }) => {
       let courseAssignments = this.studentInfo.student_assignment.filter((assignment: { assignment: { course: { id: any; }; }; }) => assignment.assignment.course.id === course.course.id);
 
       let assignmentData: any = [];
@@ -59,12 +61,24 @@ export class GradesTabComponent implements OnInit {
         formative: formative,
       })
 
-      courseAssignmentData.push({
-        course: course.course.course_code + ' ' + course.course.course_name,
-        currentGrade: course.grades.current,
-        predictedGrade: course.grades.predict,
-        assignments: assignmentData,
-      });
+      if(this.user.tutor.role==='admin'){
+        courseAssignmentData.push({
+          course: course.course.course_code + ' ' + course.course.course_name,
+          currentGrade: course.grades.current,
+          averageCurrent: course.course.average_grades.current,
+          predictedGrade: course.grades.predict,
+          averagePredicted: course.course.average_grades.predict,
+          assignments: assignmentData,
+        });
+      } else {
+        courseAssignmentData.push({
+          course: course.course.course_code + ' ' + course.course.course_name,
+          currentGrade: course.grades.current,
+          predictedGrade: course.grades.predict,
+          assignments: assignmentData,
+        });
+      }
+
     });
     console.log(courseAssignmentData);
     this.courseAssignmentInfo = courseAssignmentData;
@@ -74,23 +88,56 @@ export class GradesTabComponent implements OnInit {
     let courseLabel: string[] = [];
     let currentGrades: any[] = [];
     let predictedGrades: any[] = [];
+    let currentGradesAverage: any[] = [];
+    let predictedGradesAverage: any[] = [];
     let gradeDatasets = [];
 
-    this.courseAssignmentInfo.forEach((courseInfo: { course: string; currentGrade: any; predictedGrade: any; }) => {
+    this.courseAssignmentInfo.forEach((courseInfo: {
+      averageCurrent(averageCurrent: any): unknown;
+      averagePredicted(averagePredicted: any): unknown; course: string; currentGrade: any; predictedGrade: any; 
+}) => {
       courseLabel.push(courseInfo.course);
       currentGrades.push(courseInfo.currentGrade);
+      if(this.user.tutor.role==='admin'){
+        currentGradesAverage.push(courseInfo.averageCurrent);
+      }
       predictedGrades.push(courseInfo.predictedGrade);
+      if(this.user.tutor.role==='admin'){
+        predictedGradesAverage.push(courseInfo.averagePredicted);
+      }
     });
 
-    gradeDatasets.push(
-      {
-        label: 'Current Grades',
-        data: currentGrades
-      }, 
-      {
-        label: 'Predicted Grades',
-        data: predictedGrades
-      });
+    if(this.user.tutor.role==='admin'){
+      gradeDatasets.push(
+        {
+          label: 'Current Grades',
+          data: currentGrades
+        }, 
+        {
+          label: 'Average Current Grades',
+          data: currentGradesAverage
+        }, 
+        {
+          label: 'Predicted Grades',
+          data: predictedGrades
+        },
+        {
+          label: 'Average Predicted Grades',
+          data: predictedGradesAverage
+        },
+        );
+    } else {
+      gradeDatasets.push(
+        {
+          label: 'Current Grades',
+          data: currentGrades
+        }, 
+        {
+          label: 'Predicted Grades',
+          data: predictedGrades
+        });
+    }
+
 
     this.chart = new Chart(this.barGraph.nativeElement, {
       type: 'bar',
